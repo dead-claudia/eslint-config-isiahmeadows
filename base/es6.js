7d00039bee7a18f642ad8490aa8024ed446a3b0f
Isiah Meadows <impinball@gmail.com>
2016-01-26 02:16:44 -0500

    Wrong property disabled

--- base/es6.js
+++ base/es6.js
@@ -8,16 +8,16 @@ module.exports = {
     rules: {
         // "use strict" declarations are implicit, so this is almost a no-op.
         "strict": [2, "global"],
-
-        // This leads to a major gotcha in its buggy implementation.
-        // See https://github.com/eslint/eslint/issues/4417
-        "arrow-body-style": 0,
-
+        "arrow-body-style": 2,
         "arrow-parens": [2, "as-needed"],
         "arrow-spacing": 2,
         "constructor-super": 2,
         "generator-star-spacing": [2, "before"],
-        "no-arrow-condition": 2,
+
+        // This leads to a major gotcha in its buggy implementation.
+        // See https://github.com/eslint/eslint/issues/4417
+        "no-arrow-condition": 0,
+
         "no-class-assign": 2,
         "no-const-assign": 2,
         "no-dupe-class-members": 2,
